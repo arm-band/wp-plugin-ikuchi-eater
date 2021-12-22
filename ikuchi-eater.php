fba5de518054221a8a14204f3dbab6ac8a3a5852
arm-band <bre4tn.let@gmail.com>
2021-12-23 00:14:00 +0900

    置換条件を調整
    - 正規表現パターンにマッチしなかったら
    - または、 (パターンにはマッチするが) その投稿の投稿IDとスラッグの投稿ID部が異なる場合
    - または、 (パターンにはマッチするが) その記事タイトルが「mm/dd(d)」形式のとき、スラッグの月日部がタイトルの「mm」または「dd」と異なる場合
    
    というように条件を指定した。

--- ikuchi-eater.php
+++ ikuchi-eater.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Ikuchi Eater
 Description: 通常の投稿のみ、スラッグ決定時に機械的にスラッグを上書きする。スラッグのフォーマットは {post_type}-{post_id}-yyyy-mm-dd
-Version:     0.0.2
+Version:     0.0.3
 Author:      アルム＝バンド
 */
 
@@ -33,7 +33,7 @@ class IkuchiEater
     /**
      * mucus
      *
-     * description                : 通常の投稿のみ、スラッグ決定時に機械的にスラッグを上書きする。スラッグのフォーマットは <post_type>-<post_id>-yyyy-mm-dd (month, date は0詰めあり)
+     * description                : 通常の投稿のみ、スラッグ決定時に機械的にスラッグを上書きする。スラッグのフォーマットは {post_type}-{post_id}-yyyy-mm-dd (month, date は0詰めあり)
      *
      * @param stinrg $slug        : オリジナルのスラッグ
      * @param int    $post_ID     : 投稿ID
@@ -49,11 +49,26 @@ class IkuchiEater
         $post_type
     )
     {
-        if (
-            $post_type === 'post'
-            && !preg_match( '/^[\w\-_]+\-[\d]+\-(20|19)\d{2}\-\d{2}\-\d{2}$/i', $slug )
-        ) {
-            $slug = utf8_uri_encode( $post_type ) . '-' . $post_ID . '-' . date('Y-m-d');
+        if ( $post_type === 'post' ) {
+            $flag = preg_match( '/^[\w\-_]+\-(\d+)\-(20|19)\d{2}\-(\d{2})\-(\d{2})$/i', $slug, $matches );
+            $post = get_post( $post_ID );
+            $title_array = explode('/', $post->post_title);
+            if (
+                !$flag
+                || ( (int)$matches[1] !== $post_ID )
+            ) {
+                $slug = utf8_uri_encode( $post_type ) . '-' . $post_ID . '-' . date('Y-m-d');
+            }
+            else if (
+                isset($title_array[0]) && !empty($title_array[0])
+                && isset($title_array[1]) && !empty($title_array[1])
+                && (
+                    (int)$matches[3] !== (int)$title_array[0]
+                    || (int)$matches[4] !== (int)$title_array[1]
+                )
+            ) {
+                $slug = utf8_uri_encode( $post_type ) . '-' . $post_ID . '-' . date('Y') . '-' . $title_array[0] . '-' . $title_array[1];
+            }
         }
         return $slug;
     }
